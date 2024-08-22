b1ecf17c38a02cc210ee2736f923f180ab05537d
Veronka Avdeeva <veron.avdeeva2015@yandex.ru>
2024-08-22 14:07:52 +0300

    create Navbar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+  <MantineProvider>
       <App />
+      </MantineProvider>
   </BrowserRouter>
 )
