c8a68ed464e05e7b86a9756c2b500ca251e9de2f
Veronka Avdeeva <veron.avdeeva2015@yandex.ru>
2024-08-23 15:44:53 +0300

    done

--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -11,39 +11,38 @@ router.get("/", verifyAccessToken, async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
-router.get("/:id", verifyAccessToken, async (req, res) => {
-  const { id } = req.params;
+// router.get("/", verifyAccessToken, async (req, res) => {
+//   const { id } = req.params;
 
-  try {
-    const accountOne = await Account.findOne({ where: { id} });
-    res.status(200).json(accountOne);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-router.post("/:id",verifyAccessToken, async (req, res) => {
-    const { id } = req.params;
-  const {idUser, photo, country, city, phone, about } = req.body;
+//   try {
+//     const accountOne = await Account.findOne({ where: { id} });
+//     res.status(200).json(accountOne);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+// router.post("/:id",verifyAccessToken, async (req, res) => {
+//     const { id } = req.params;
+//   const {idUser, photo, country, city, phone, about } = req.body;
   
-  try {
-    const accountCreate = await Account.create({idUser:id, photo, country, city, phone, about } );
+//   try {
+//     const accountCreate = await Account.create({idUser:id, photo, country, city, phone, about } );
    
 
-    res.status(200).json(accountCreate.dataValues);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-router.put("/:id", verifyAccessToken, async (req, res) => {
+//     res.status(200).json(accountCreate.dataValues);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+router.put("/", verifyAccessToken, async (req, res) => {
     try {
      const { user}  = res.locals.user 
-      const {  idUser, photo, country, city, phone, about }  =
+      const {   photo, country, city, phone, about }  =
         req.body;
-      const { id } = req.params;
   
       const [updateStatus] = await Account.update(
-          { idUser, photo, country, city, phone, about } ,
-        { where: { id, idUser } }
+          {  photo, country, city, phone, about } ,
+        { where: { idUser:user.id } }
       );
   
       updateStatus ? res.sendStatus(200) : res.sendStatus(404);
@@ -51,18 +50,18 @@ router.put("/:id", verifyAccessToken, async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
-router.delete("/:id", verifyAccessToken,async (req, res) => {
+// router.delete("/:id", verifyAccessToken,async (req, res) => {
 
-  try {
-    const { id } = req.params;
-    const user = res.locals.user
+//   try {
+//     const { id } = req.params;
+//     const user = res.locals.user
 
-    const deleteStatus = await Account.destroy({ where: { id, idUser: user.id } });
+//     const deleteStatus = await Account.destroy({ where: { id, idUser: user.id } });
 
-    deleteStatus ? res.sendStatus(200) : res.sendStatus(404);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+//     deleteStatus ? res.sendStatus(200) : res.sendStatus(404);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 module.exports = router;
--- server/routes/roles.routes.js
+++ server/routes/roles.routes.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+    const router = require("express").Router();
 const verifyAccessToken = require("../middleware/verifyAccessToken");
 const { Role } = require("../db/models");
 
