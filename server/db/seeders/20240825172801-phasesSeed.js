7fb4f20b009db3435e02fa855a4467ba8cb1d393
beAngelFace <be.angelface@gmail.com>
2024-08-26 13:59:48 +0300

    feat:seed db,events &e.t.c.

--- server/db/seeders/20240825172801-phasesSeed.js
+++ server/db/seeders/20240825172801-phasesSeed.js
@@ -3,23 +3,29 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+   
+     await queryInterface.bulkInsert('Phases', [{
+    phase: '0',
+     description: 'Все самое лучшее только начинается'
+     },
+     {
+    phase: '1',
+    description: 'Это база, братья и сестры!'
+       },
+       {
+    phase: '2',
+    description: 'Ты думаешь, что ты понимаешь, но ты не понимаешь, а когда ты не понимаешь, то внезапно понимаешь'
+         },
+         {
+    phase: '3',
+    description: 'Финишная прямая. Что такое "сон"?'
+           }], {});
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+     await queryInterface.bulkDelete('Phases', null, {});
+     
   }
 };
--- server/db/seeders/20240825172948-dataBaseSeed.js
+++ server/db/seeders/20240825172948-dataBaseSeed.js
@@ -3,23 +3,25 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+     await queryInterface.bulkInsert('DataBases', [{
+      title: "Lorem ipsum dolor",
+      description: "Lorem ipsum dolor  labore et",
+      pic: "https://ucare.timepad.ru/94755349-3020-4096-9bbf-ed47223c1877/poster_event_1255976.jpg",
+      userId: 8
+     },
+     {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+      pic: "https://www.softo-mir.ru/wp-content/uploads/2021/07/original_5d3a15dcea08b6189e1e7176_6038e707af962.jpg",
+      userId: 9
+     }], {});
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+     await queryInterface.bulkDelete('DataBases', null, {});
+     
   }
 };
